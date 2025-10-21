def6fc646c6fa13a27ec9bf0edc72fde8bc20348
murali4421 <58593175+murali4421@users.noreply.github.com>
2025-10-21 20:11:14 +0530

    feat: Remove unused enableProdMode call
    The `enableProdMode()` function is no longer necessary as Angular's default production mode behavior is automatically applied. Removing this call simplifies the application bootstrapping logic.

--- index.tsx
+++ index.tsx
@@ -3,12 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
 */
 import '@angular/compiler';
-import {enableProdMode, provideZonelessChangeDetection} from '@angular/core';
+import {provideZonelessChangeDetection} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 
 import {AppComponent} from './src/app/app.component';
-
-enableProdMode();
 
 bootstrapApplication(AppComponent, {
   providers: [provideZonelessChangeDetection()],
